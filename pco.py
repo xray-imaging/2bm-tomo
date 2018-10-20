f8c0a384fb5a4724f65fe9175cffa9284f54e589
Francesco De Carlo <decarlof@gmail.com>
2018-10-19 19:25:00 -0500

    first commit, testek OK except FrameType

---
+++ pco.py
@@ -0,0 +1,249 @@
+import time
+import epics
+import numpy as np
+import os
+import Tkinter
+import tkMessageBox as mbox
+
+from edge_lib import *
+
+
+global variableDict
+
+variableDict = {'PreDarkImages': 20,
+        'PreWhiteImages': 20,
+        'Projections': 1500,
+        'PostDarkImages': 0,
+        'PostWhiteImages': 0,
+        'SampleXIn': 0.0,
+        'SampleXOut': 5,
+        'SampleStartPos': 0.0,
+        'SampleEndPos': 180.0,
+        'StartSleep_min': 0,
+        'ExposureTime': 0.1,
+        'ExposureTime_flat': 0.1,
+        'ShutterOpenDelay': 0.00,
+        'IOC_Prefix': '2bmbPG3:', # other supported detectors: 'PCOIOC2:', 'PCOIOC3:', '2bmbSP1:'
+        'FileWriteMode': 'Stream',
+        'CCD_Readout': 0.05
+        }
+
+global_PVs = {}
+
+
+def EdgeMultiPosScan(exposureTime=0.1, slewSpeed=1, angStart=0, angEnd = 180,
+                numProjPerSweep=1500,
+                samInPos=0, samOutDist=4,
+                roiSizeX = 2560, roiSizeY = 1240,
+                posStep = 0, posNum = 1, 
+                delay = 0, flatPerScan = 1, darkPerScan = 1,
+                accl = 1, shutterMode=0, scanMode=0, clShutter=1, white_ang=90):                      
+    """ Multiple poistion scans along vertical direction with edge camera
+                
+      axisShift: rotation axis shift in unit um/mm. it is defined as the shift distance when the vertical stage moves up.
+              it assumes rotation axis is at image center at posInit.
+    """
+              
+##### AHutch tomo configurations -- start
+    if shutterMode == 0:
+        camShutterMode = "Rolling"
+    elif shutterMode == 1:
+        camShutterMode = "Global"
+    else:
+        print "Wrong camera shutter mode! Quit ..."
+        return False   
+    if scanMode == 0:
+        camScanSpeed = 'Normal'
+    elif scanMode == 1:
+        camScanSpeed = 'Fast'
+    elif scanMode == 2:
+        camScanSpeed = 'Fastest'
+    else:
+        print "Wrong camera scan mode! Quit ..."  
+        return False                
+                                  
+    camPrefix = "PCOIOC3"                
+    shutter = "2bma:A_shutter"          
+
+    samStage = "2bma:m49"
+    posStage = "2bma:m20"        
+    rotStage = "2bma:m82"
+    PSO = "2bma:PSOFly2"
+                
+    if samStage == '2bma:m49':                
+        initEdge(samInPos = samInPos,samStage=samStage,rotStage=rotStage)      
+    elif samStage == '2bma:m20':
+        samInPos = epics.caget(samStage+".VAL")
+        initEdge(samInPos = samInPos,samStage=samStage,rotStage=rotStage)  
+    else:
+        print "Please edit the script to define samInPos. Quit"
+        exit()
+
+    if camPrefix == 'PCOIOC3':
+        cam = 'edge'
+    elif camPrefix == 'PCOIOC2':
+        cam = 'dimax'    
+
+    if samStage.split(':')[0] == '2bma':
+        station = 'AHutch'
+    elif samStage.split(':')[0] == '2bmb':    
+        station = 'BHutch'                                
+
+    posInit = epics.caget(posStage+".VAL")
+    epics.caput(PSO + ":scanControl.VAL","Standard", wait=True, timeout=1000.0)                
+    scanDelta = 1.0*(angEnd-angStart)/numProjPerSweep
+    acclTime = 1.0*slewSpeed/accl
+    frate = int(1.0*numProjPerSweep/(1.0*(angEnd-angStart)/slewSpeed) + 5)                
+
+    print("Freme Rate:", frate)
+    numImage = numProjPerSweep
+    if clShutter==1:        
+        numImage += 20
+    elif clShutter==0:
+        if flatPerScan == 1:
+            numImage += 10
+        if darkPerScan == 1: 
+            numImage += 10
+    else:
+        print 'Wrong clShutter value. Quit!'
+        exit()
+    
+    # test camera -- start
+    edgeTest(camScanSpeed, camShutterMode, roiSizeX, roiSizeY)
+                
+    # sample scan -- start
+    print "start sample scan ... "
+    for ii in range(posNum):    
+    # set scan parameters -- start 
+        setPSO(slewSpeed, scanDelta, acclTime, angStart=angStart, angEnd=angEnd, PSO=PSO,rotStage=rotStage)            
+        timestamp = [x for x in time.asctime().rsplit(' ') if x!='']                                 
+
+        edgeSet(numImage, exposureTime, frate, PSO = PSO)
+        epics.caput(shutter+":open.VAL",1, wait=True, timeout=1000.0)
+        epics.caput("2bma:m23.VAL","1", wait=True, timeout=1000.0)                                    
+        time.sleep(3)                                                            
+        edgeAcquisition(samInPos,samStage,numProjPerSweep,shutter,PSO = PSO,rotStage=rotStage)
+        epics.caput(camPrefix + ":cam1:Acquire.VAL","Done", wait=True, timeout=1000.0)    
+        print "scan at position #",ii+1," is done!"
+
+        samOutPos = samInPos + samOutDist
+        
+        if flatPerScan == 1:
+        # set for white field -- start                   
+            print "Acquiring flat images ..."   
+            epics.caput(rotStage+'.VAL',white_ang)                
+            edgeAcquireFlat(samInPos,samOutPos,samStage,rotStage,shutter, PSO = PSO)       
+            epics.caput(camPrefix + ":cam1:Acquire.VAL","Done", wait=True, timeout=1000.0)  
+            epics.caput(rotStage+'.VAL',0)               
+            print "flat for position #", ii+1, " is done!"                
+        # set for white field -- end                                            
+                                
+        if darkPerScan == 1:
+            # set for dark field -- start
+            print "Acquiring dark images ..."                
+            edgeAcquireDark(samInPos,samStage,rotStage,shutter, PSO = PSO) 
+            epics.caput(camPrefix + ":cam1:Acquire.VAL","Done", wait=True, timeout=1000.0)             
+            print "dark is done!"  
+        if  posNum!=1 and darkPerScan!=0 and flatPerScan!=0 and ii!=(posNum-1):       
+            epics.caput(camPrefix + ":HDF1:Capture.VAL","Done", wait=True, timeout=1000.0)
+        # set for dark field -- end
+                                        
+        epics.caput(posStage + ".VAL",str(posInit+(ii+1)*posStep), wait=True, timeout=1000.0)
+        if ii != (posNum-1):
+            time.sleep(delay)
+                                          
+    print "sample scan is done!"          
+                                
+
+
+    logFile = open(logFileName,'a')
+    logFile.write("Scan was done at time: " + time.asctime() + '\n')
+    logFile.close()                                                                    
+                         
+    # sample scan -- end
+
+    if clShutter==1:        
+        if flatPerScan == 0:    
+            # set for white field -- start                   
+            print "Acquiring flat images ..."    
+            epics.caput(shutter+":open.VAL",1, wait=True, timeout=1000.0)    
+            time.sleep(3)
+            epics.caput(rotStage+'.VAL',white_ang)  
+            print   samInPos,samOutPos               
+            edgeAcquireFlat(samInPos,samOutPos,samStage,rotStage,shutter, PSO = PSO) 
+            epics.caput(camPrefix + ":cam1:Acquire.VAL","Done", wait=True, timeout=1000.0) 
+            epics.caput(rotStage+'.VAL',0)   
+    #        epics.caput(camPrefix + ":HDF1:Capture.VAL","Done", wait=True, timeout=1000.0)         
+            print "flat is done!"                
+            # set for white field -- end
+    
+        if darkPerScan == 0:    
+            # set for dark field -- start
+            print "Acquiring dark images ..."                
+            edgeAcquireDark(samInPos,samStage,rotStage,shutter, PSO = PSO) 
+            epics.caput(camPrefix + ":cam1:Acquire.VAL","Done", wait=True, timeout=1000.0)    
+    #        epics.caput(camPrefix + ":HDF1:Capture.VAL","Done", wait=True, timeout=1000.0)           
+            print "dark is done!" 
+            
+        epics.caput(shutter+":close.VAL",1, wait=True, timeout=1000.0)      
+        epics.caput("2bma:m23.VAL","1", wait=True, timeout=1000.0)      
+
+    epics.caput(camPrefix + ":HDF1:Capture.VAL","Done", wait=True, timeout=1000.0)
+    epics.caput(BL+":caputRecorderGbl_10.VAL",epics.caget(camPrefix + ":HDF1:FileNumber.VAL",as_string=True), wait=True, timeout=1000.0)
+    # set for dark field -- end
+    
+    # set for new scans -- start
+    epics.caput(camPrefix+":cam1:pcoedge_fastscan.VAL","Normal", wait=True, timeout=1000.0)
+    epics.caput(camPrefix + ":cam1:pco_trigger_mode.VAL","Auto", wait=True, timeout=1000.0)
+    epics.caput(camPrefix + ":cam1:ImageMode.VAL","Continuous", wait=True, timeout=1000.0)    
+    epics.caput(camPrefix + ":cam1:SizeX.VAL",str(roiSizeX), wait=True, timeout=1000.0)
+    epics.caput(camPrefix + ":cam1:SizeY.VAL",str(roiSizeY), wait=True, timeout=1000.0)                                                                               
+    # set for new scans -- end
+    print "Scan finished!" 
+    epics.caput(posStage + ".VAL",str(posInit), wait=True, timeout=1000.0)
+    
+def main():
+    #EdgeMultiPosScan() 
+    exposureTime = 0.1
+    roiSizeX = 2560
+    roiSizeY = 1240
+    PSO = "2bma:PSOFly2"
+    rotStage = "2bma:m82"
+    samStage = '2bma:m49'
+    samInPos = 0
+    shutter = "2bma:A_shutter"      
+    slewSpeed=1
+    camPrefix = "PCOIOC3"                
+
+    angStart=0
+    angEnd = 18
+    numProjPerSweep=150
+    scanDelta = 1.0*(angEnd-angStart)/numProjPerSweep
+
+    accl = 1
+    acclTime = 1.0*slewSpeed/accl
+     
+    frate = int(1.0*numProjPerSweep/(1.0*(angEnd-angStart)/slewSpeed) + 5)                
+    numImage = numProjPerSweep + 20
+
+    samOutDist = 3
+    samOutPos = samInPos + samOutDist
+
+    epics.caput(camPrefix + ":cam1:FrameType.ZRST", "/exchange/data")
+    epics.caput(camPrefix + ":cam1:FrameType.ONST", "/exchange/data_dark")
+    epics.caput(camPrefix + ":cam1:FrameType.TWST", "/exchange/data_white")
+             
+    initEdge(samInPos,samStage,rotStage)      
+    edgeTest('Normal', "Rolling", roiSizeX, roiSizeY)
+    setPSO(slewSpeed, scanDelta, acclTime, angStart, angEnd, PSO, rotStage)
+    edgeSet(numImage, exposureTime, frate, PSO = PSO)
+    edgeAcquisition(samInPos, samStage, numProjPerSweep, shutter, PSO=PSO,rotStage=rotStage)
+    epics.caput(camPrefix + ":cam1:Acquire.VAL","Done", wait=True, timeout=1000.0)             
+    edgeAcquireFlat(samInPos, samOutPos, samStage, rotStage, shutter, PSO) 
+#    edgeAcquireFlat(samInPos, samStage, rotStage, shutter, PSO) 
+    epics.caput(camPrefix + ":cam1:Acquire.VAL","Done", wait=True, timeout=1000.0)             
+    edgeAcquireDark(samInPos, samStage, rotStage, shutter, PSO) 
+    epics.caput(camPrefix + ":cam1:Acquire.VAL","Done", wait=True, timeout=1000.0)             
+    
+if __name__ == '__main__':
+    main()
---
+++ pco_lib.py
@@ -0,0 +1,205 @@
+import time
+import epics
+
+def setPSO(slewSpeed, scanDelta, acclTime,angStart=0, angEnd=180, PSO="2bmb:PSOFly1",rotStage="2bma:m82"):
+    print(' ')
+    print('  *** Set PSO')
+    epics.caput(PSO+":startPos.VAL",str(angStart), wait=True, timeout=1000.0)                
+    epics.caput(PSO+":endPos.VAL",str(angEnd), wait=True, timeout=1000.0)
+    epics.caput(rotStage+".VELO",str(slewSpeed), wait=True, timeout=1000.0)
+    epics.caput(PSO+":slewSpeed.VAL",str(slewSpeed), wait=True, timeout=1000.0)
+    epics.caput(rotStage+".ACCL",str(acclTime), wait=True, timeout=1000.0)
+    epics.caput(PSO+":scanDelta.VAL",str(scanDelta), wait=True, timeout=1000.0)    
+    print('  *** Set PSO: Done!')
+
+
+def initEdge(samInPos=0, samStage=None, rotStage=None):
+    print(' ')
+    print('  *** Init PCO')
+    camPrefix = "PCOIOC3"                            
+    shutter = "2bma:A_shutter"
+    if samStage is None:
+        samStage = "2bma:m49"  
+    if rotStage is None:              
+        rotStage = "2bma:m82"   
+    epics.caput("2bma:m23.VAL","0", wait=True, timeout=1000.0)                
+    epics.caput(shutter+":open.VAL",1, wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":HDF1:EnableCallbacks.VAL",1, wait=True, timeout=1000.0)   
+    epics.caput(camPrefix+":HDF1:Capture.VAL","Done", wait=True, timeout=1000.0) 
+    epics.caput(camPrefix+":HDF1:NumCaptured_RBV.VAL","0", wait=True, timeout=1000.0)    
+    epics.caput(camPrefix+":cam1:Acquire.VAL","Done", wait=True, timeout=1000.0)    
+    epics.caput(camPrefix+":cam1:pco_trigger_mode.VAL","Auto", wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":cam1:ImageMode.VAL","Continuous", wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":cam1:pco_edge_fastscan.VAL","Normal", wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":cam1:pco_is_frame_rate_mode.VAL",0, wait=True, timeout=1000.0)    
+    epics.caput(camPrefix+":cam1:AcquireTime.VAL",0.2, wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":image1:EnableCallbacks.VAL","Enable", wait=True, timeout=1000.0)
+    epics.caput(rotStage+".STOP",1, wait=True, timeout=1000.0)
+    epics.caput(rotStage+".SET","Set", wait=True, timeout=1000.0) 
+    epics.caput(rotStage+".VAL",epics.caget(rotStage+".VAL")%360.0, wait=True, timeout=1000.0) 
+    epics.caput(rotStage+".SET","Use", wait=True, timeout=1000.0) 
+    epics.caput(rotStage+".VELO","30", wait=True, timeout=1000.0)    
+    epics.caput(rotStage+".ACCL","3", wait=True, timeout=1000.0)                
+    epics.caput(rotStage+".VAL","0", wait=True, timeout=1000.0)
+    if samInPos is not None:
+        epics.caput(samStage+".VAL",str(samInPos), wait=True, timeout=1000.0)  
+    epics.caput("2bma:m23.VAL","1", wait=True, timeout=1000.0)               
+    print('  *** Init PCO: Done!')
+
+
+def edgeSet(numImage, exposureTime, frate,PSO = "2bmb:PSOFly1"):    
+    print(' ')
+    print('  *** Set PCO')
+    camPrefix = "PCOIOC3"
+    epics.caput(camPrefix+":cam1:pco_is_frame_rate_mode.VAL","DelayExp", wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":cam1:AcquirePeriod.VAL","0", wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":cam1:pco_set_frame_rate.VAL",str(frate+1), wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":cam1:pco_set_frame_rate.VAL",str(frate), wait=True, timeout=1000.0)                    
+    epics.caput(camPrefix+":HDF1:AutoIncrement.VAL","Yes", wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":HDF1:NumCapture.VAL",str(numImage), wait=True, timeout=1000.0)                
+    epics.caput(camPrefix+":HDF1:NumCapture_RBV.VAL",str(numImage), wait=True, timeout=1000.0)  
+    epics.caput(camPrefix+":HDF1:NumCaptured_RBV.VAL","0", wait=True, timeout=1000.0)                
+#    epics.caput(camPrefix+":HDF1:FilePath.VAL",filepath, wait=True, timeout=1000.0)
+#    epics.caput(camPrefix+":HDF1:FileName.VAL",filename, wait=True, timeout=1000.0)    
+    epics.caput(camPrefix+":HDF1:FileTemplate.VAL","%s%s_%4.4d.hdf", wait=True, timeout=1000.0)                
+    epics.caput(camPrefix+":HDF1:AutoSave.VAL","Yes", wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":HDF1:FileWriteMode.VAL","Stream", wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":HDF1:Capture.VAL","Capture", wait=False, timeout=1000.0)
+    epics.caput(camPrefix+":cam1:NumImages.VAL",str(numImage), wait=True, timeout=1000.0)                                
+    epics.caput(camPrefix+":cam1:ImageMode.VAL","Multiple", wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":cam1:AcquireTime.VAL",str(exposureTime), wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":cam1:pco_trigger_mode.VAL","Soft/Ext", wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":cam1:pco_ready2acquire.VAL","0", wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":cam1:Acquire.VAL","Acquire", wait=False, timeout=1000.0)            
+    print('  *** Set PCO: Done!')
+
+
+def edgeTest(camScanSpeed,camShutterMode,roiSizeX=2560,roiSizeY=2160):
+    camPrefix = "PCOIOC3"
+    print(' ')
+    print('  *** Testing PCO camera')
+    epics.caput(camPrefix+":cam1:ArrayCallbacks.VAL","Enable", wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":cam1:NumImages.VAL","10", wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":cam1:ImageMode.VAL","Multiple", wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":cam1:pco_global_shutter.VAL",camShutterMode, wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":cam1:pco_edge_fastscan.VAL",camScanSpeed, wait=True, timeout=1000.0)                
+    epics.caput(camPrefix+":cam1:AcquireTime.VAL","0.001000", wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":cam1:SizeX.VAL",str(roiSizeX), wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":cam1:SizeY.VAL",str(roiSizeY), wait=True, timeout=1000.0)
+    epics.caput(camPrefix+":cam1:pco_trigger_mode.VAL","Auto", wait=True, timeout=1000.0)    
+    epics.caput(camPrefix+":cam1:Acquire.VAL","Acquire", wait=True, timeout=1000.0)     
+    print('  *** Testing PCO camera: Done!')
+
+def edgeAcquisition(samInPos, samStage, numProjPerSweep, shutter, clShutter=1, PSO="2bma:PSOFly2", rotStage="2bma:m82"):
+    print(' ')
+    print('  *** Acquisition')
+    print('      *** Projections')
+    camPrefix = "PCOIOC3"    
+
+##    epics.caput(shutter+":open.VAL",1, wait=True, timeout=1000.0)                
+    epics.caput(camPrefix+":cam1:FrameType.VAL",'0', wait=True, timeout=1000.0)     
+    print("Type Projections: ", epics.caget(camPrefix+":cam1:FrameType.VAL"))
+    epics.caput(samStage+".VAL",str(samInPos), wait=True, timeout=1000.0)
+    
+    rotCurrPos = epics.caget(rotStage + ".VAL")
+    epics.caput(rotStage + ".SET",str(1), wait=True, timeout=1000.0)       
+    epics.caput(rotStage + ".VAL",str(1.0*rotCurrPos%360.0), wait=True, timeout=1000.0) 
+    epics.caput(rotStage + ".SET",str(0), wait=True, timeout=1000.0)  
+    
+    epics.caput(rotStage + ".VELO","50.00000", wait=True, timeout=1000.0)
+    epics.caput(rotStage + ".VAL","0.00000", wait=False, timeout=1000.0)   
+                 
+    epics.caput(PSO+":taxi.VAL","Taxi", wait=True, timeout=1000.0)
+    epics.caput(PSO+":fly.VAL","Fly", wait=True, timeout=1000.0) 
+##    if epics.caget(PSO+":fly.VAL") == 0 & clShutter == 1:               
+##        epics.caput(shutter+":close.VAL",1, wait=True, timeout=1000.0)  
+        
+    rotCurrPos = epics.caget(rotStage + ".VAL")
+    epics.caput(rotStage + ".SET",str(1), wait=True, timeout=1000.0)       
+    epics.caput(rotStage + ".VAL",str(1.0*rotCurrPos%360.0), wait=True, timeout=1000.0) 
+    epics.caput(rotStage + ".SET",str(0), wait=True, timeout=1000.0) 
+             
+    epics.caput(rotStage + ".VELO","50.00000", wait=True, timeout=1000.0)
+    time.sleep(1)
+    epics.caput(rotStage + ".VAL","0.00000", wait=False, timeout=1000.0)   
+    while (epics.caget(camPrefix+":HDF1:NumCaptured_RBV.VAL") != epics.caget(camPrefix+":cam1:NumImagesCounter_RBV.VAL")):      
+        time.sleep(1)                    
+    print('      *** Projections: Done!')
+
+
+def edgeAcquireFlat(samInPos,samOutPos,samStage,rotStage, shutter, PSO = "2bma:PSOFly2"):    
+    print('      *** White Fields')
+    camPrefix = "PCOIOC3"
+    epics.caput(samStage+".VAL",str(samOutPos), wait=True, timeout=1000.0)                
+    epics.caput(PSO + ":scanControl.VAL","Standard", wait=True, timeout=1000.0)                
+##    epics.caput(shutter+":open.VAL",1, wait=True, timeout=1000.0)
+    time.sleep(5)
+    epics.caput(camPrefix+":cam1:FrameType.VAL",'2', wait=True, timeout=1000.0)     
+    print("Type White: ", epics.caget(camPrefix+":cam1:FrameType.VAL"))
+    epics.caput(camPrefix+":cam1:NumImages.VAL","10", wait=True, timeout=1000.0)   
+    
+    epics.caput(camPrefix+":cam1:pco_trigger_mode.VAL","Auto", wait=True, timeout=1000.0)   
+    epics.caput(camPrefix+":cam1:TriggerMode","Internal", wait=True, timeout=1000.0)            
+    time.sleep(5)            
+    epics.caput(camPrefix+":cam1:Acquire.VAL","Acquire", wait=True, timeout=1000.0)  
+    
+##    epics.caput(shutter+":close.VAL",1, wait=True, timeout=1000.0)
+    time.sleep(5)            
+    epics.caput(camPrefix+":cam1:Acquire.VAL","Done", wait=True, timeout=1000.0)
+    epics.caput(samStage+".VAL",str(samInPos), wait=True, timeout=1000.0)                    
+
+
+
+def edgeAcquireDark(samInPos,samStage,rotStage, shutter, PSO = "2bma:PSOFly2"):    
+    print("      *** Dark Fields")
+    camPrefix = "PCOIOC3"    
+    epics.caput(PSO + ":scanControl.VAL","Standard", wait=True, timeout=1000.0)
+##    epics.caput(shutter+":close.VAL",1, wait=True, timeout=1000.0)
+    time.sleep(5)
+            
+    epics.caput(camPrefix+":cam1:FrameType.VAL",'1', wait=True, timeout=1000.0)             
+    print("Type Dark: ", epics.caget(camPrefix+":cam1:FrameType.VAL"))
+    
+    epics.caput(camPrefix+":cam1:NumImages.VAL","10", wait=True, timeout=1000.0)
+
+    epics.caput(camPrefix+":cam1:pco_trigger_mode.VAL","Auto", wait=True, timeout=1000.0)            
+##    epics.caput(camPrefix+":cam1:TriggerMode","Internal", wait=True, timeout=1000.0)            
+    epics.caput(camPrefix+":cam1:Acquire.VAL","Acquire", wait=True, timeout=1000.0)
+        
+    epics.caput(camPrefix+":cam1:Acquire.VAL","Done", wait=True, timeout=1000.0)    
+    epics.caput(samStage+".VAL",str(samInPos), wait=True, timeout=1000.0)    
+    print('      *** Dark Fileds: Done!')
+    print('  *** Acquisition: Done!')
+
+def init_general_PVs(global_PVs, variableDict):
+
+    # shutter pv's
+    global_PVs['ShutterA_Open'] = PV('2bma:A_shutter:open.VAL')
+    global_PVs['ShutterA_Close'] = PV('2bma:A_shutter:close.VAL')
+    global_PVs['ShutterA_Move_Status'] = PV('PA:02BM:STA_A_FES_OPEN_PL')
+    global_PVs['ShutterB_Open'] = PV('2bma:B_shutter:open.VAL')
+    global_PVs['ShutterB_Close'] = PV('2bma:B_shutter:close.VAL')
+    global_PVs['ShutterB_Move_Status'] = PV('PA:02BM:STA_B_SBS_OPEN_PL')
+
+    if STATION == '2-BM-A':
+            print('*** Running in station A:')
+            # Set sample stack motor pv's:
+            global_PVs['Motor_SampleX'] = PV('2bma:m49.VAL')
+            global_PVs['Motor_SampleY'] = PV('2bma:m20.VAL')
+            global_PVs['Motor_SampleRot'] = PV('2bma:m82.VAL')  
+            global_PVs['Motor_SampleRot_Stop'] = PV('2bma:m82.STOP') # Aerotech ABR-250
+            global_PVs['Motor_Sample_Top_X'] = PV('2bma:m50.VAL')
+            global_PVs['Motor_Sample_Top_Z'] = PV('2bma:m51.VAL') 
+            # Set FlyScan
+            global_PVs['Fly_ScanDelta'] = PV('2bma:PSOFly2:scanDelta')
+            global_PVs['Fly_StartPos'] = PV('2bma:PSOFly2:startPos')
+            global_PVs['Fly_EndPos'] = PV('2bma:PSOFly2:endPos')
+            global_PVs['Fly_SlewSpeed'] = PV('2bma:PSOFly2:slewSpeed')
+            global_PVs['Fly_Taxi'] = PV('2bma:PSOFly2:taxi')
+            global_PVs['Fly_Run'] = PV('2bma:PSOFly2:fly')
+            global_PVs['Fly_ScanControl'] = PV('2bma:PSOFly2:scanControl')
+            global_PVs['Fly_Calc_Projections'] = PV('2bma:PSOFly2:numTriggers')
+            global_PVs['Theta_Array'] = PV('2bma:PSOFly2:motorPos.AVAL')
+
+        
+ 

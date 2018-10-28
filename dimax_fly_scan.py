956f43e2cedef2a2965cdb8e0281a5726f508236
Francesco De Carlo <decarlof@gmail.com>
2018-10-27 19:19:25 -0500

    triggers fly scan but does not save yet

--- dimax_fly_scan.py
+++ dimax_fly_scan.py
@@ -12,9 +12,9 @@ global variableDict
 
 variableDict = {'PreDarkImages': 0,
         'PreWhiteImages': 0,
-        'Projections': 1500,
-        'PostDarkImages': 20,
-        'PostWhiteImages': 20,
+        'Projections': 15,
+        'PostDarkImages': 16,
+        'PostWhiteImages': 17,
         'SampleXIn': 0.0,
         'SampleXOut': 5,
         'SampleRotStart': 0.0,
@@ -30,7 +30,7 @@ variableDict = {'PreDarkImages': 0,
         'AcclRot': 1.0,
         'EnergyPink': 2.657, # for now giver in mirror angle in rads
         'EnergyMono': 24.9,
-        'Station': '2-BM-B'
+        'Station': '2-BM-A'
 #        'camScanSpeed': 'Normal', # options: 'Normal', 'Fast', 'Fastest'
 #        'camShutterMode': 'Rolling'# options: 'Rolling', 'Global''
         }
@@ -38,32 +38,49 @@ variableDict = {'PreDarkImages': 0,
 global_PVs = {}
 
 
-                
 def main():
 
     tic =  time.time()
     update_variable_dict(variableDict)
     init_general_PVs(global_PVs, variableDict)
+    
+    if(0==0):
+    ##try: 
+        model = global_PVs['Cam1_Model'].get()
+        if model == None:
+            print('*** The PCO Camera with EPICS IOC prefix %s is down' % variableDict['IOC_Prefix'])
+            print('  *** Failed!')
+        else:
+            print ('*** The %s is on' % (model))            # get sample file name
+            dimaxInit(global_PVs, variableDict)     
+            dimaxTest(global_PVs, variableDict)
 
-    dimaxInit(global_PVs, variableDict)
-##    dimaxTest(global_PVs, variableDict)
-    # setPSO(global_PVs, variableDict)              
+            fname = global_PVs['HDF1_FileName'].get(as_string=True)
+            print('  *** File name prefix: %s' % fname)
+            
+            dimaxSet(global_PVs, variableDict, fname)
 
-##    fname = global_PVs['HDF1_FileName'].get(as_string=True)
-##    print('  *** File name prefix: %s' % fname)
-##    dimaxSet(global_PVs, variableDict, fname)
-    dimaxAcquireFlat(global_PVs, variableDict)
+            setPSO(global_PVs, variableDict)
 
-    # set scan parameters -- end
-    # open_shutters(global_PVs, variableDict)
-    # dimaxAcquisition(global_PVs, variableDict)                        
-    # dimaxAcquireFlat(global_PVs, variableDict) 
-    # close_shutters(global_PVs, variableDict)
-    # dimaxAcquireDark(global_PVs, variableDict) 
-    print(' ')
-    print('  *** Total scan time: %s minutes' % str((time.time() - tic)/60.))
-    print('  *** Data file: %s' % global_PVs['HDF1_FullFileName_RBV'].get(as_string=True))
-    print('  *** Done!')
+            open_shutters(global_PVs, variableDict)
+            dimaxAcquisition(global_PVs, variableDict)
+            time.sleep(2)                
+
+            dimaxAcquireFlat(global_PVs, variableDict)  
+            global_PVs['Cam1_PCODumpCameraMemory'].put(1, wait=True, timeout=1000.0)             
+            
+            close_shutters(global_PVs, variableDict)
+            dimaxAcquireDark(global_PVs, variableDict)
+            global_PVs['Cam1_PCODumpCameraMemory'].put(1, wait=True, timeout=1000.0)             
+
+            print(' ')
+            print('  *** Total scan time: %s minutes' % str((time.time() - tic)/60.))
+            print('  *** Data file: %s' % global_PVs['HDF1_FullFileName_RBV'].get(as_string=True))
+            print('  *** Done!')
+
+    ##except  KeyError:
+    ##    print('  *** Some PV assignment failed!')
+    ##    pass
 
      
 if __name__ == '__main__':

2020bb35b144b9a502a387db2631a4aace2ec6ea
Francesco De Carlo <decarlof@gmail.com>
2018-11-14 00:35:43 -0600

    fist commit

---
+++ dimax_reset.py
@@ -0,0 +1,74 @@
+import time
+import numpy as np
+import os
+import Tkinter
+import tkMessageBox as mbox
+
+from pco_lib import *
+
+global variableDict
+
+variableDict = {
+        'ExposureTime': 0.0005,
+        'SlewSpeed': 180.0,
+        'AcclRot': 180.0,
+        'SampleRotStart': 0.0,
+        'SampleRotEnd': 180.0,
+        'Projections': 1500,
+        'SampleXIn': 0.0,
+        'SampleXOut': 10,
+        'roiSizeX': 2016, 
+        'roiSizeY': 600,        
+        'PostWhiteImages': 20,
+        'PostDarkImages': 20,
+        'ShutterOpenDelay': 0.00,
+        'IOC_Prefix': 'PCOIOC2:', # options: 1. DIMAX: 'PCOIOC2:', 2. EDGE: 'PCOIOC3:'
+        'FileWriteMode': 'Stream',
+        'CCD_Readout': 0.05,
+        'EnergyPink': 2.657, # for now giver in mirror angle in rads
+        'EnergyMono': 24.9,
+        'Station': '2-BM-A',
+        'StartSleep_min': 0,#        'camScanSpeed': 'Normal', # options: 'Normal', 'Fast', 'Fastest'
+#        'camShutterMode': 'Rolling'# options: 'Rolling', 'Global''
+        }
+
+global_PVs = {}
+
+
+def main():
+
+    tic =  time.time()
+    update_variable_dict(variableDict)
+    init_general_PVs(global_PVs, variableDict)
+    
+    try: 
+        model = global_PVs['Cam1_Model'].get()
+        if model == None:
+            print('*** The PCO Camera with EPICS IOC prefix %s is down' % variableDict['IOC_Prefix'])
+            print('  *** Failed!')
+        else:
+            print ('*** The %s is on' % (model))            # get sample file name
+            dimaxInit(global_PVs, variableDict)     
+
+            dimaxTest(global_PVs, variableDict)
+
+            print('  *** Done!')
+
+    except  KeyError:
+        print('  *** Some PV assignment failed!')
+        pass
+
+     
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+

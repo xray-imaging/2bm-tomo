3f6e961534b55781eb43044d4ea7388f9f6905e6
Francesco De Carlo <decarlof@gmil.comwq>
2019-04-02 08:47:13 -0500

    2019-04 - Thampy - First commit

--- flir_fly_scan.py
+++ flir_fly_scan.py
@@ -20,7 +20,7 @@ global variableDict
 
 variableDict = {
         'SampleXIn': 0.0, 
-        'SampleXOut': -11,
+        'SampleXOut': -10,
         # 'SampleYIn': 0,                 # to use Y change the sampleInOutVertical = True
         # 'SampleYOut': -4,
         'SampleInOutVertical': False,     # False: use X to take the white field

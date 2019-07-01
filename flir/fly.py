bd8e343a6502e437ea40c276ac815cdaf50eaeef
Francesco De Carlo <decarlof@gmil.comwq>
2019-07-01 13:00:47 -0500

    2019-07 Demortiere - start

--- flir/fly.py
+++ flir/fly.py
@@ -28,7 +28,7 @@ global variableDict
 
 variableDict = {
         'SampleXIn': 0, 
-        'SampleXOut': -9,
+        'SampleXOut': 3,
         # 'SampleYIn': 0,                 # to use Y change the sampleInOutVertical = True
         # 'SampleYOut': -4,
         'SampleInOutVertical': False,     # False: use X to take the white field

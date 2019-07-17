1a5f0d313cf897a3c33d87e27df6c63f812d086a
Francesco De Carlo <decarlof@gmil.comwq>
2019-07-17 12:54:47 -0500

    2019-07 -- Wolff start

--- flir/fly_vertical_scan.py
+++ flir/fly_vertical_scan.py
@@ -27,11 +27,11 @@ import libs.dm_lib as dm_lib
 global variableDict
 
 variableDict = {
-        'StartY': 23.9,
-        'EndY': 24.1,
-        'StepSize': 1.6,
-        'SampleXIn': 0.0,
-        'SampleXOut': 19,
+        'StartY': 35.18,
+        'EndY': 35.88,
+        'StepSize': 0.7,
+        'SampleXIn': 0,
+        'SampleXOut': 5,
         # 'SampleYIn': 0,                 # to use Y change the sampleInOutVertical = True
         # 'SampleYOut': -4,
         'SampleInOutVertical': False,     # False: use X to take the white field
@@ -42,7 +42,7 @@ variableDict = {
         'NumWhiteImages': 20,
         'NumDarkImages': 20,
         # ####################### DO NOT MODIFY THE PARAMETERS BELOW ###################################
-        'CCD_Readout': 0.006,             # options: 1. 8bit: 0.006, 2. 16-bit: 0.01
+        'CCD_Readout': 0.02,             # options: 1. 8bit: 0.006, 2. 16-bit: 0.01
         # 'CCD_Readout': 0.01,             # options: 1. 8bit: 0.006, 2. 16-bit: 0.01
         'Station': '2-BM-A',
         'ExposureTime': 0.01,             # to use this as default value comment the variableDict['ExposureTime'] = global_PVs['Cam1_AcquireTime'].get() line

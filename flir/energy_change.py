4f66788eb6d403f597181a99791b1e756f29808d
Francesco De Carlo <decarlof@gmail.com>
2019-09-12 00:03:50 -0500

    working in progress ... adding energy change function

--- flir/energy_change.py
+++ flir/energy_change.py
@@ -12,24 +12,26 @@ import libs.log_lib as log_lib
 global variableDict
 
 variableDict = {
-        'Projections': 1500,
-        'NumDarkImages': 20,
-        'NumWhiteImages': 20,
-        'SampleXIn': 0.0,
-        'SampleXOut': 5,
-        'SampleStartPos': 0.0,
-        'SampleEndPos': 180.0,
-        'StartSleep_min': 0,
-        'SlewSpeed': 1.0,
-        'ExposureTime': 0.1,
-        'ExposureTime_flat': 0.1,
-        'ShutterOpenDelay': 0.00,
-        'IOC_Prefix': 'PCOIOC3:', # options: 'PCOIOC2:', 'PCOIOC3:'
-        'FileWriteMode': 'Stream',
-        'CCD_Readout': 0.05,
-        'AcclRot': 1.0,
-        'EnergyPink': 2.657, # for now giver in mirror angle in rads
-        'EnergyMono': 24.9
+        'Station': '2-BM-A',
+        'IOC_Prefix': '2bmbSP1:',         # options: 1. PointGrey: '2bmbPG3:', 2. Gbe '2bmbSP1:' 
+        # 'Projections': 1500,
+        # 'NumDarkImages': 20,
+        # 'NumWhiteImages': 20,
+        # 'SampleXIn': 0.0,
+        # 'SampleXOut': 5,
+        # 'SampleStartPos': 0.0,
+        # 'SampleEndPos': 180.0,
+        # 'StartSleep_min': 0,
+        # 'SlewSpeed': 1.0,
+        # 'ExposureTime': 0.1,
+        # 'ExposureTime_flat': 0.1,
+        # 'ShutterOpenDelay': 0.00,
+        # 'IOC_Prefix': 'PCOIOC3:', # options: 'PCOIOC2:', 'PCOIOC3:'
+        # 'FileWriteMode': 'Stream',
+        # 'CCD_Readout': 0.05,
+        # 'AcclRot': 1.0,
+        # 'EnergyPink': 2.657, # for now giver in mirror angle in rads
+        # 'EnergyMono': 24.9
 #        'camScanSpeed': 'Normal', # options: 'Normal', 'Fast', 'Fastest'
 #        'camShutterMode': 'Rolling'# options: 'Rolling', 'Global''
         }

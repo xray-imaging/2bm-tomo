9e6795ef31b6736391365e006e3cf48b671994c8
Francesco De Carlo <decarlof@gmai.com>
2020-01-18 14:05:28 -0600

    fixing errors

--- tomo2bm/scan.py
+++ tomo2bm/scan.py
@@ -26,30 +26,30 @@ global_PVs = {}
 def fly_sleep(params):
 
     tic =  time.time()
-    # aps2bm.update_variable_dict(variableDict)
-    global_PVs = aps2bm.init_general_PVs(global_PVs, variableDict)
+    # aps2bm.update_variable_dict(params)
+    global_PVs = aps2bm.init_general_PVs(global_PVs, params)
     try: 
         detector_sn = global_PVs['Cam1_SerialNumber'].get()
         if ((detector_sn == None) or (detector_sn == 'Unknown')):
-            log.info('*** The Point Grey Camera with EPICS IOC prefix %s is down' % variableDict['IOC_Prefix'])
+            log.info('*** The Point Grey Camera with EPICS IOC prefix %s is down' % params.ioc_prefix)
             log.info('  *** Failed!')
         else:
             log.info('*** The Point Grey Camera with EPICS IOC prefix %s and serial number %s is on' \
-                        % (variableDict['IOC_Prefix'], detector_sn))
+                        % (params.ioc_prefix, detector_sn))
             
             # calling global_PVs['Cam1_AcquireTime'] to replace the default 'ExposureTime' with the one set in the camera
             params.exposure_time = global_PVs['Cam1_AcquireTime'].get()
             # calling calc_blur_pixel() to replace the default 'SlewSpeed' 
-            blur_pixel, rot_speed, scan_time = aps2bm.calc_blur_pixel(global_PVs, variableDict)
+            blur_pixel, rot_speed, scan_time = aps2bm.calc_blur_pixel(global_PVs, params)
             params.slew_speed = rot_speed
 
             # init camera
-            aps2bm.pgInit(global_PVs, variableDict)
+            aps2bm.pgInit(global_PVs, params)
 
             # set sample file name
             fname = str('{:03}'.format(global_PVs['HDF1_FileNumber'].get())) + '_' + global_PVs['Sample_Name'].get(as_string=True)
 
-            scan_lib.tomo_fly_scan(global_PVs, variableDict, fname)
+            scan_lib.tomo_fly_scan(global_PVs, params, fname)
 
             log.info(' ')
             log.info('  *** Total scan time: %s minutes' % str((time.time() - tic)/60.))
@@ -61,7 +61,7 @@ def fly_sleep(params):
 
             global_PVs['Cam1_ImageMode'].put('Continuous')
 
-            dm_lib.scp(global_PVs, variableDict)
+            dm_lib.scp(global_PVs, params)
 
             log.info('  *** Done!')
 
@@ -70,56 +70,55 @@ def fly_sleep(params):
         pass
 
 
-
-def dummy_tomo_fly_scan(global_PVs, variableDict, fname):
+def dummy_tomo_fly_scan(global_PVs, params, fname):
     log.info(' ')
     log.info('  *** start_scan')
 
     def cleanup(signal, frame):
-        aps2bm.stop_scan(global_PVs, variableDict)
+        aps2bm.stop_scan(global_PVs, params)
         sys.exit(0)
     signal.signal(signal.SIGINT, cleanup)
 
     
-def tomo_fly_scan(global_PVs, variableDict, fname):
+def tomo_fly_scan(global_PVs, params, fname):
     log.info(' ')
     log.info('  *** start_scan')
 
     def cleanup(signal, frame):
-        aps2bm.stop_scan(global_PVs, variableDict)
+        aps2bm.stop_scan(global_PVs, params)
         sys.exit(0)
     signal.signal(signal.SIGINT, cleanup)
 
-    if variableDict.has_key('StopTheScan'):
-        aps2bm.stop_scan(global_PVs, variableDict)
+    if params.has_key('StopTheScan'):
+        aps2bm.stop_scan(global_PVs, params)
         return
 
     # moved to outer loop in main()
-    # pgInit(global_PVs, variableDict)
+    # pgInit(global_PVs, params)
 
-    aps2bm.setPSO(global_PVs, variableDict)
+    aps2bm.setPSO(global_PVs, params)
 
     # fname = global_PVs['HDF1_FileName'].get(as_string=True)
     log.info('  *** File name prefix: %s' % fname)
 
-    aps2bm.pgSet(global_PVs, variableDict, fname) 
+    aps2bm.pgSet(global_PVs, params, fname) 
 
-    aps2bm.open_shutters(global_PVs, variableDict)
+    aps2bm.open_shutters(global_PVs, params)
 
     # # run fly scan
-    theta = aps2bm.pgAcquisition(global_PVs, variableDict)
+    theta = aps2bm.pgAcquisition(global_PVs, params)
 
     theta_end =  global_PVs['Motor_SampleRot_RBV'].get()
     if (0 < theta_end < 180.0):
         # print('\x1b[2;30;41m' + '  *** Rotary Stage ERROR. Theta stopped at: ***' + theta_end + '\x1b[0m')
         log.error('  *** Rotary Stage ERROR. Theta stopped at: %s ***' % str(theta_end))
 
-    aps2bm.pgAcquireFlat(global_PVs, variableDict)
-    aps2bm.close_shutters(global_PVs, variableDict)
+    aps2bm.pgAcquireFlat(global_PVs, params)
+    aps2bm.close_shutters(global_PVs, params)
     time.sleep(2)
 
-    aps2bm.pgAcquireDark(global_PVs, variableDict)
+    aps2bm.pgAcquireDark(global_PVs, params)
 
-    aps2bm.checkclose_hdf(global_PVs, variableDict)
+    aps2bm.checkclose_hdf(global_PVs, params)
 
-    aps2bm.add_theta(global_PVs, variableDict, theta)
+    aps2bm.add_theta(global_PVs, params, theta)

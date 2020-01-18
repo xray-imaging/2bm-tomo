dd606941d13f08ed009c431c0a06d45e0a11372e
Francesco De Carlo <decarlof@gmai.com>
2020-01-18 14:56:44 -0600

    fixing errors

--- tomo2bm/dm.py
+++ tomo2bm/dm.py
@@ -44,19 +44,19 @@ def create_remote_directory(remote_server, remote_dir):
         return -1
 
 
-def scp(global_PVs, variableDict):
+def scp(global_PVs, params):
 
     log.info(' ')
     log.info('  *** Data transfer')
 
-    remote_server = variableDict['RemoteAnalysisDir'].split(':')[0]
-    remote_top_dir = variableDict['RemoteAnalysisDir'].split(':')[1]
+    remote_server = params.remote_analysis_dir.split(':')[0]
+    remote_top_dir = params.remote_analysis_dir.split(':')[1]
     log.info('      *** remote server: %s' % remote_server)
     log.info('      *** remote top directory: %s' % remote_top_dir)
 
     fname_origin = global_PVs['HDF1_FullFileName_RBV'].get(as_string=True)
     p = pathlib.Path(fname_origin)
-    fname_destination = variableDict['RemoteAnalysisDir'] + p.parts[-3] + '/' + p.parts[-2] + '/'
+    fname_destination = params.remote_analysis_dir + p.parts[-3] + '/' + p.parts[-2] + '/'
     remote_dir = remote_top_dir + p.parts[-3] + '/' + p.parts[-2] + '/'
 
     log.info('      *** origin: %s' % fname_origin)
@@ -86,3 +86,4 @@ def mkdir(remote_server, remote_dir):
     ret = dm_lib.check_remote_directory(remote_server , remote_dir)
     if ret == 2:
         iret = dm_lib.create_remote_directory(remote_server, remote_dir)
+

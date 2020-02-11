6d63acecde0e7ff7bbe0a0e85a293af2a1fd1ea3
Viktor Nikitin <decarlof@gmil.comwq>
2020-02-11 16:50:56 -0600

    DEV: Add autocomplete for tomo scan command

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
 
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):        
+        install.run(self)
+        from tomo2bm.auto_complete import create_complete_scan
+        import pathlib
+        fname = os.path.expanduser("~")+'/complete_tomo.sh'        
+        create_complete_scan.run(fname)
+        print('For autocomplete please run: \n\n $ source '+ fname +'\n')
 setup(
     name='tomo2bm',
     version=open('VERSION').read().strip(),
@@ -12,4 +23,5 @@ setup(
     scripts=['bin/tomo'],
     description='cli to run tomo scans at 2-bm',
     zip_safe=False,
+    cmdclass={'install': PostInstallCommand}
 )

98d1b8e55252dffd29912497b8790aa58afe7af6
Viktor Nikitin <decarlof@gmil.comwq>
2020-02-17 15:06:54 -0600

    fix auto-complete for python 3

--- tomo2bm/auto_complete/create_complete_scan.py
+++ tomo2bm/auto_complete/create_complete_scan.py
@@ -8,15 +8,14 @@ def run(fname):
                                stdout=subprocess.PIPE,
                                stderr=subprocess.STDOUT)
         stdout, _ = out.communicate()
-        #stdout = str(stdout)  #Python 3
+        stdout = str(stdout)
 
         cmdscan = []
         parscan = []
         st = stdout.find("optional")
         while(1):
             st = stdout.find('--', st)
-            end = min(stdout.find(' ', st),stdout.find('\n', st))
-            #end = min(stdout.find(' ', st),stdout.find('\n', st))  #Python 3
+            end = min(stdout.find(' ', st),stdout.find('\\n', st))
             if(st < 0):
                 break            
             cmdscan.append(stdout[st:end])            
@@ -24,24 +23,22 @@ def run(fname):
             st1 = stdout.find('--', end)                
             if(st1>st or st1<0):
                 end = stdout.find(')', st)
-                parscan.append(stdout[st+9:end].replace(" ","").replace("\n",""))                
-                #parscan.append(stdout[st+9:end].replace(" ","").replace("\\n",""))         #Python 3       
+                parscan.append(stdout[st+9:end].replace(" ","").replace("\\n",""))                
             else:
-                parscan.append("")                 
-            st = end
-                    
+                parscan.append("") 
+            st = end        
         # Create bash file
         fid = open(fname, 'w')
         fid.write(
             '#/usr/bin/env bash\n _tomo()\n{\n\tlocal cur prev opts\n\tCOMPREPLY=()\n\tcur="${COMP_WORDS[COMP_CWORD]}"\n\tprev="${COMP_WORDS[COMP_CWORD-1]}"\n')
 
         # check all tomo scan
-        fid.write('\tif [[ ${prev} == "scan" ]] ; then\n')
+        fid.write('\tif [[ ${COMP_WORDS[1]} == "scan" ]] ; then\n')
         fid.write('\t\topts="')
         fid.write(' '.join(cmdscan))
         fid.write('"\n')
         fid.write(
-            '\t\tCOMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )\n\t\treturn 0\n\tfi\n')
+            '\t\tCOMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )\n\tfi\n')
 
         for k in range(len(cmdscan)):
             fid.write('\tif [[ ${prev} == "')

97a86e97d56933b101bfd472d8abd3fb0939c1fc
Francesco De Carlo <decarlof@gmil.comwq>
2020-03-22 15:46:42 -0500

    added license

--- tomo2bm/sphere.py
+++ tomo2bm/sphere.py
@@ -1,6 +1,55 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# #########################################################################
+# Copyright (c) 2019-2020, UChicago Argonne, LLC. All rights reserved.    #
+#                                                                         #
+# Copyright 2019-2020. UChicago Argonne, LLC. This software was produced  #
+# under U.S. Government contract DE-AC02-06CH11357 for Argonne National   #
+# Laboratory (ANL), which is operated by UChicago Argonne, LLC for the    #
+# U.S. Department of Energy. The U.S. Government has rights to use,       #
+# reproduce, and distribute this software.  NEITHER THE GOVERNMENT NOR    #
+# UChicago Argonne, LLC MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR        #
+# ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.  If software is     #
+# modified to produce derivative works, such modified software should     #
+# be clearly marked, so as not to confuse it with the version available   #
+# from ANL.                                                               #
+#                                                                         #
+# Additionally, redistribution and use in source and binary forms, with   #
+# or without modification, are permitted provided that the following      #
+# conditions are met:                                                     #
+#                                                                         #
+#     * Redistributions of source code must retain the above copyright    #
+#       notice, this list of conditions and the following disclaimer.     #
+#                                                                         #
+#     * Redistributions in binary form must reproduce the above copyright #
+#       notice, this list of conditions and the following disclaimer in   #
+#       the documentation and/or other materials provided with the        #
+#       distribution.                                                     #
+#                                                                         #
+#     * Neither the name of UChicago Argonne, LLC, Argonne National       #
+#       Laboratory, ANL, the U.S. Government, nor the names of its        #
+#       contributors may be used to endorse or promote products derived   #
+#       from this software without specific prior written permission.     #
+#                                                                         #
+# THIS SOFTWARE IS PROVIDED BY UChicago Argonne, LLC AND CONTRIBUTORS     #
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT       #
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS       #
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL UChicago     #
+# Argonne, LLC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,        #
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,    #
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;        #
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER        #
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT      #
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN       #
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         #
+# POSSIBILITY OF SUCH DAMAGE.                                             #
+# #########################################################################
+
+"""
+Module for sphere alignment.
+"""
+
 import sys
 import json
 import time
@@ -88,9 +137,6 @@ def adjust(params):
 
 def adjust_center(global_PVs,params,dark_field,white_field):
 
-    print(params.verbose)
-    exit()
-
     log.warning(' *** Adjusting center ***')              
     for ang in [params.adjust_center_angle_1, params.adjust_center_angle_2]: 
         log.warning('  *** take 3 spheres angular %f deg ***' % float(ang))
@@ -149,7 +195,10 @@ def adjust_center(global_PVs,params,dark_field,white_field):
         cmass_0 = center_of_mass(sphere_0)
         log.warning('  *** CHECK: center of mass for the sphere at 0 deg (%f,%f) ***' % (cmass_0[1],cmass_0[0]))
         
-def adjust_roll(global_PVs,params, dark_field, white_field,angle_shift):
+def adjust_roll(global_PVs,params, dark_field, white_field, angle_shift):
+
+    # angle_shift is the correction that is needed to apply to the rotation axis position
+    # to align the Z stage on top of the rotary stage with the beam
 
     log.warning(' *** Adjusting roll ***')
     log.info('  *** moving rotary stage to %f deg position ***' % float(0+angle_shift))                                                
